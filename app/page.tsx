11d28eee4e2c888ed95afbcbec5023f2dbf38c47
Левчук Анастасия <anilukin.yatsan@yandex.ru>
2026-02-04 11:18:37 +0000

    add button

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,51 @@
-import Image from "next/image";
+'use client';
+
+import React, { useState } from 'react';
+import { Form, InputGroup, Button } from 'react-bootstrap';
 
 export default function Home() {
+  const [walletNumber, setWalletNumber] = useState('');
+
+  const handleSubmit = (e: React.SubmitEvent) => {
+    e.preventDefault();
+    if (!walletNumber.trim()) {
+      return;
+    }
+    //TODO: wallet connection
+  };
+  const handleClear = () => setWalletNumber('');
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
+    <div className='container m-3'>
+      <div className='row'>
+        <Form onSubmit={handleSubmit} className='mb-4'>
+          <InputGroup className='mb-3'>
+            <Form.Control
+              placeholder='Enter wallet number'
+              aria-label='wallet number'
+              aria-describedby='connectWallet'
+              name='walletNumber'
+              id='walletNumberField'
+              type='text'
+              autoComplete='off'
+              value={walletNumber}
+              onChange={(e) => setWalletNumber(e.target.value)}
             />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+            {walletNumber && (
+              <Button
+                variant='outline-secondary-light'
+                onClick={handleClear}
+                type='button'
+                className='border-0'
+              >
+                ✕
+              </Button>
+            )}
+            <Button variant='primary' id='connectWallet'>
+              Connect Wallet
+            </Button>
+          </InputGroup>
+        </Form>
+      </div>
     </div>
   );
 }
